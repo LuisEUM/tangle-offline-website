189fba0e1a72e1b088d053f9a7a85648176eba63
¨LuisEUM¨ <luiseum95@gmail.com>
2023-01-09 22:45:13 +0100

    Section 2 - In process... Logos Done

--- app/components/sections/SectionTwo.jsx
+++ app/components/sections/SectionTwo.jsx
@@ -33,7 +33,7 @@ export default function SectionTwo () {
           <motion.div className='  '>
             <div>
               <div className='flex gap-12'>
-                <div className='bg- flex justify-center content-center  align-middle relative h-[400px]'>
+                <div className='bg- flex justify-center content-center  align-middle relative h-[450px]'>
                   <div className='bg- flex justify-center content-center  align-middle gap-6'>
                     <div className=' flex h-[63px] ml-0.5 bg-tangle-rich-black-FOGBRA-29 rounded-full  px-5 py-10 self-start mt-10' style={{ background: 'radial-gradient(50% 50% at 50% 50%, rgba(13, 17, 28, 1) 85%, rgba(13, 17, 28, 0.6) 100%)' }}>
                       <Ellipse icon='heart' className='h-16 p-12 my-5 max-w-[30px] ' />
@@ -45,7 +45,7 @@ export default function SectionTwo () {
                         </motion.div>
                         <motion.div>
                           <h3 className='mt-2 mr-2 text-3xl md:text-6xl lg:text-6xl font-bold '>We bring <span className='text-tangle-cyan-process'>thousands</span>  <br /> of people out to <em>{first}</em></h3>
-                          <div className='grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 max-h-[64px] mt-8'>
+                          <div className='grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 max-h-[64px] mt-11'>
                             <div className='self-center'>
                               <Image src='/logos/merchantLogo4.png' alt='Logo The Amsterdam Dungeon' width={181} height={38} className='max-w-full max-h-full m-auto' />
                             </div>
@@ -64,7 +64,7 @@ export default function SectionTwo () {
                     </div>
                   </div>
                   <div className='bg-yellow- flex absolute -z-30 left-5 top-0'>
-                    <VerticalLine width={42} heigth={400} />
+                    <VerticalLine width={42} heigth={450} />
                   </div>
                 </div>
               </div>
