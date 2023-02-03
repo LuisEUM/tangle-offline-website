a1fa269ff8e28b7c69adbbee65f924004ca7452e
¨LuisEUM¨ <luiseum95@gmail.com>
2023-02-03 03:32:31 +0100

    Fixing deployment

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import Head from './head'
 import '../styles/globals.css'
 import { LanguageProvider } from './context/languageContext'
 import Footer from './components/footer/Footer'
-import NavBar from './components/navbar/NavBar'
+import NavBar from './components/nav-mobile/NavBar'
 
 export default function RootLayout ({ children }) {
   return (
