85e717a970f07332ea649c0cf5b7dbb7c32d785b
¨LuisEUM¨ <luiseum95@gmail.com>
2023-02-05 18:23:31 +0100

    Fixing video on Safari

--- app/components/video-player/VideoPlayer.jsx
+++ app/components/video-player/VideoPlayer.jsx
@@ -1,8 +1,8 @@
 'use client'
 export default function VideoPlayer ({ text }) {
   return (
-    <div className='mx-2 md:mx-14 lg:mx-32 aspect-video rounded-3xl overflow-hidden flex'>
-      <div className=' max-full aspect-video'>
+    <div className='mx-2 md:mx-14 lg:mx-32 aspect-video rounded-3xl overflow-hidden flex max-w-full'>
+      <div className=' max-full aspect-video w-full'>
         <iframe className='aspect-video' width='100%' height='100%' src='https://www.youtube.com/embed/PXO5Uig6qg8' title={text.tittle} allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen />
       </div>
     </div>
